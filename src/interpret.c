8682112bc3f2fac3e12a6a851a1784c2a0be5d6f
sanakan <xdkekekek@gmail.com>
2023-01-08 03:37:57 +1100

    back to pc

--- src/interpret.c
+++ src/interpret.c
@@ -1,6 +1,8 @@
 #include "interpret.h"
 
 #include "log.h"
+#include "map.h"
+#include <stdlib.h>
 
 typedef enum {
   SPEC_I32,
@@ -22,51 +24,62 @@ typedef struct {
 typedef struct var_s {
   type_t        type;
   expr_t        expr;
-  const char    *ident;
   struct var_s  *next;
 } var_t;
 
 static void expr_print(const expr_t *expr);
 
-static void   int_stmt(const s_node_t *node);
-static void   int_decl(const s_node_t *node);
-static type_t int_type(const s_node_t *node);
-static expr_t int_expr(const s_node_t *node);
-static expr_t int_binop(const s_node_t *node);
-static expr_t int_constant(const s_node_t *node);
+static void   int_stmt(map_t scope, const s_node_t *node);
+static void   int_decl(map_t scope, const s_node_t *node);
+static type_t int_type(map_t scope, const s_node_t *node);
+static expr_t int_expr(map_t scope, const s_node_t *node);
+static expr_t int_binop(map_t scope, const s_node_t *node);
+static expr_t int_constant(map_t scope, const s_node_t *node);
 
 void interpret(const s_node_t *node)
 {
+  map_t scope = map_new();
   if (node)
-    int_stmt(node);
+    int_stmt(scope, node);
 }
 
-void int_stmt(const s_node_t *node)
+void int_stmt(map_t scope, const s_node_t *node)
 {
   expr_t expr;
   switch (node->stmt.body->node_type) {
   case S_BINOP:
   case S_CONSTANT:
-    expr = int_expr(node->stmt.body);
+    expr = int_expr(scope, node->stmt.body);
     expr_print(&expr);
     break;
   case S_DECL:
-    int_decl(node);
+    int_decl(scope, node->stmt.body);
     break;
   }
+  
+  if (node->stmt.next)
+    int_stmt(scope, node->stmt.next);
 }
 
-void int_decl(const s_node_t *node)
+void int_decl(map_t scope, const s_node_t *node)
 {
-  type_t type = int_type(node->decl.type);
-  var_t *var = 
+  if (map_get(scope, node->decl.ident->data.ident)) {
+    LOG_DEBUG("redefinition of '%s'");
+    return;
+  }
+  
+  var_t *var = malloc(sizeof(var_t));
+  var->type = int_type(scope, node->decl.type);
+  var->expr = int_expr(scope, node->decl.init);
+  
+  map_put(scope, node->decl.ident->data.ident, var);
 }
 
-type_t int_type(const s_node_t *node)
+type_t int_type(map_t scope, const s_node_t *node)
 {
   type_t type;
   switch (node->type.spec->token) {
-  case TK_I32;
+  case TK_I32:
     type.spec = SPEC_I32;
     break;
   case TK_F32:
@@ -77,20 +90,20 @@ type_t int_type(const s_node_t *node)
   return type;
 }
 
-expr_t int_expr(const s_node_t *node)
+expr_t int_expr(map_t scope, const s_node_t *node)
 {
   switch (node->node_type) {
   case S_BINOP:
-    return int_binop(node);
+    return int_binop(scope, node);
   case S_CONSTANT:
-    return int_constant(node);
+    return int_constant(scope, node);
   }
 }
 
-expr_t int_binop(const s_node_t *node)
+expr_t int_binop(map_t scope, const s_node_t *node)
 {
-  expr_t lhs = int_expr(node->binop.lhs);
-  expr_t rhs = int_expr(node->binop.rhs);
+  expr_t lhs = int_expr(scope, node->binop.lhs);
+  expr_t rhs = int_expr(scope, node->binop.rhs);
   
   expr_t expr;
   if (lhs.type.spec == SPEC_I32 && rhs.type.spec == SPEC_I32) {
@@ -132,8 +145,9 @@ expr_t int_binop(const s_node_t *node)
   return expr;
 }
 
-expr_t int_constant(const s_node_t *node)
+expr_t int_constant(map_t scope, const s_node_t *node)
 {
+  var_t *var;
   expr_t expr;
   switch (node->constant.lexeme->token) {
   case TK_CONST_INTEGER:
@@ -144,6 +158,9 @@ expr_t int_constant(const s_node_t *node)
     expr.type.spec = SPEC_F32;
     expr.f32 = node->constant.lexeme->data.f32;
     break;
+  case TK_IDENTIFIER:
+    var = map_get(scope, node->constant.lexeme->data.ident);
+    expr = var->expr;
   }
   
   return expr;
