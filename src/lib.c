07531fad5c7ffc2ed8e3e83d0bdceeaca104d8c3
sanakan <xdkekekek@gmail.com>
2023-01-25 18:42:29 +1100

    i forgot to add stuff

--- src/lib.c
+++ src/lib.c
@@ -1,6 +1,7 @@
 #include "lib.h"
 
 #include "data.h"
+#include "mem.h"
 #include "int_main.h"
 #include <math.h>
 #include <stdio.h>
@@ -50,10 +51,27 @@ bool sin_f(expr_t *ret_value, scope_t *scope_args)
   expr_f32(ret_value, sin(theta.f32));
 }
 
+bool input_f(expr_t *ret_value, scope_t *scope_args)
+{
+  expr_t prompt;
+  int_arg_load(scope_args, &prompt, "prompt");
+  
+  printf("%s", prompt.block->block);
+  
+  char str_input[256];
+  scanf("%256s", str_input);
+  
+  ret_value->type = type_string;
+  ret_value->block = heap_alloc_string(str_input);
+  ret_value->loc_base = NULL;
+  ret_value->loc_offset = 0;
+}
+
 void lib_load_stdlib()
 {
   int_bind("getch", getch_f);
   int_bind("clear", clear_f);
+  int_bind("input", input_f);
 }
 
 void lib_load_math()
